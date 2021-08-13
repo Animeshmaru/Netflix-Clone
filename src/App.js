ad19ee346c2ef5fb4ea52b597ecfbd1ed2ce6b24
Animeshmaru <animeshmaru16@gmail.com>
2021-08-13 13:49:55 +0530

    added netflix-clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import Row from './Row';
+import requests from './requests'
+import Navbar from './Navbar'
+import Banner from './Banner'
+import Fotter from './Fotter'
+import Download from './Download'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        {/* navbar */}
+        <Navbar />
+        {/* banner */}
+        <Banner />
+      <Download/>
+      <Row title="Netflix Originals" 
+      fetchUrl={requests.fetchNetflixOriginals} 
+      isLarge/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Romantic" fetchUrl={requests.fetchRomanticMovies}/>
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies}/>
+      {/* <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries}/> */}
+      <Fotter/>
+    </>
+  )
 }
-
 export default App;
